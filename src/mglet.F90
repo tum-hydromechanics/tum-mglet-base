603647f19a0b587853493b51f135210d029180bb
Simon Wenczowski <simon.wenczowski@tum.de>
2023-12-17 17:57:41 +0100

    restoring state of mglet.F90 by removing test changes

--- src/mglet.F90
+++ src/mglet.F90
@@ -8,8 +8,6 @@ PROGRAM main
     USE timeloop_mod, ONLY: init_timeloop, finish_timeloop, timeloop
     USE scalar_mod, ONLY: init_scalar, finish_scalar
 
-    WRITE(*,*) "Hello World"
-
     ! Initialization of core data structures
     CALL init_core()
 
