06c1bee338213a8d2f7769e69df1f9b8494b0747
Jakob <97621617+jafudev@users.noreply.github.com>
2024-05-13 20:08:05 +0200

    Adds bash script to run benchmarks easily

---
+++ benchmark/run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+ACTION=$1
+CASE=$2
+
+USAGE_EXAMPLE="./run.sh <bench|clean> <case 8|64> [processes] [mglet-binary-path]"
+
+# Determine case (8 or 64 grids)
+if [[ $CASE != 8 ]] && [[ $CASE != 64 ]]; then
+    echo "Invalid argument: No benchmark for $CASE grids. [use 8 or 64]"
+    echo "Usage example:"
+    echo $USAGE_EXAMPLE
+    exit 1
+fi
+
+# Set directory
+BENCHMARK="$CASE-grids-32-cells"
+cd $BENCHMARK
+
+if [[ "$ACTION" == "bench" ]]; then
+    NUM_PROC=${3-1}
+    MGLET_BIN="../${4:-"../build/src/mglet"}"
+    echo "Running benchmark $BENCHMARK"
+    echo "Using mglet binary $MGLET_BIN"
+    mpirun -N $NUM_PROC $MGLET_BIN 2>&1 | tee mglet.OUT
+elif [[ "$ACTION" == "clean" ]]; then
+    echo "Cleaning up files for benchmark $BENCHMARK"
+    rm -rf LOGS fields.h5 mglet-perf-report.txt *.OUT
+else
+    echo "Invalid action: $ACTION"
+    exit 1
+fi
