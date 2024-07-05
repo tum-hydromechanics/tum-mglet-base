747c9a998d0bfb6b40f8452643fe9fc8b3496509
Julius Gollor <julius.gollor@gmail.com>
2024-07-05 09:38:24 +0200

    ...

--- src/particle/particle_config_mod.F90
+++ src/particle/particle_config_mod.F90
@@ -8,14 +8,14 @@ LOGICAL :: dsim_particles = .TRUE.
 
 LOGICAL :: dread_particles = .TRUE.
 
-LOGICAL :: dinterp_particles = .TRUE.
+LOGICAL :: dinterp_particles = .FALSE.
 
 LOGICAL :: dwrite_particles = .TRUE.
 
 INTEGER(intk) :: init_npart = 100
 INTEGER(intk) :: max_np_per_list = 1000
 
-INTEGER(intk) :: psnapshot_step = 2
+INTEGER(intk) :: psnapshot_step = 1
 
 REAL(realk), PARAMETER :: D = 0.0_realk ! (3_realk) / (10_realk ** 9_realk) ! Diffusion constant in m²/s (homogeneous and isotropic diffusion for now)
 
