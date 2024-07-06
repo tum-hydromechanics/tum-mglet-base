8d840a39b55e18153807b39afe399ffde9215d05
Julius Gollor <julius.gollor@gmail.com>
2024-07-06 21:30:38 +0200

    particle config added to parameters json file for particle testcases

--- src/particle/particle_config_mod.F90
+++ src/particle/particle_config_mod.F90
@@ -33,20 +33,20 @@ CONTAINS
 
         CALL fort7%get(pconf, "/particles")
 
-        CALL pconf%get_value("/read_particles", dread_particles, .FALSE.)
+        CALL pconf%get_value("/read", dread_particles, .FALSE.)
 
-        CALL pconf%get_value("/interpolate_particles", dinterp_particles, .FALSE.)
+        CALL pconf%get_value("/interp", dinterp_particles, .FALSE.)
 
-        CALL pconf%get_value("/write_particles", dwrite_particles, .TRUE.)
+        CALL pconf%get_value("/write", dwrite_particles, .TRUE.)
 
-        CALL pconf%get_value("/init_npart_pp", init_npart, 100_intk)
+        CALL pconf%get_value("/init_np", init_npart, 100_intk)
 
         IF (init_npart <= 0_intk) THEN
             WRITE(*, *) "Initial Number of Particles must be positve. Using default Number instead."
             init_npart = 100_intk
         END IF
 
-        CALL pconf%get_value("/list_length_pp", plist_len, 1000_intk)
+        CALL pconf%get_value("/list_len", plist_len, 1000_intk)
 
         IF (plist_len <= 0_intk) THEN
             WRITE(*, *) "Particle List Length must be positve. Using default Number instead."
