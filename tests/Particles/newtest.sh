d5c21581e62bd6304211867ec42834bc59dda4e8
Julius Gollor <julius.gollor@gmail.com>
2024-07-09 10:45:57 +0200

    renamed particle testcases

---
+++ tests/Particles/newtest.sh
@@ -0,0 +1,18 @@
+cwd=$(pwd)
+
+cd ~/tum-mglet-base/tests/Particles/Advection1
+./run.sh newtest ../../../build/src/mglet
+
+cd ~/tum-mglet-base/tests/Particles/Advection2
+./run.sh newtest ../../../build/src/mglet
+
+cd ~/tum-mglet-base/tests/Particles/Advection3
+./run.sh newtest ../../../build/src/mglet
+
+cd ~/tum-mglet-base/tests/Particles/AdvectionDiffusion1
+./run.sh newtest ../../../build/src/mglet
+
+cd ~/tum-mglet-base/tests/Particles/Diffusion1
+./run.sh newtest ../../../build/src/mglet
+
+cd $cwd
