f3f65bde062adf69060e8a7c5ea76df8998927c6
Simon Wenczowski <simon.wenczowski@tum.de>
2024-09-13 11:53:31 +0200

    Transferring work from javier/statistics_uvw

--- src/scalar/scastat_mod.F90
+++ src/scalar/scastat_mod.F90
@@ -19,12 +19,200 @@ CONTAINS
             IF (LEN_TRIM(scalar(l)%name) + 4 > nchar_name) THEN
                 CALL errr(__FILE__, __LINE__)
             END IF
+            !Zeroth order
             CALL register_statfield(TRIM(scalar(l)%name)//"_AVG", comp_avg)
-        END DO
-    END SUBROUTINE init_scastat
 
+            ! basis for second central moments
+
+            ! TT_AVG
+            CALL register_statfield(TRIM(scalar(l)%name)//TRIM(scalar(l)%name)&
+            //"_AVG", comp_sqr_avg) 
+
+            ! TxTx_AVG
+            CALL register_statfield(TRIM(scalar(l)%name)//"x"//TRIM(scalar(l)%name)&
+            //"x"//"_AVG", comp_txtx_avg)
+            ! TyTy_AVG
+            CALL register_statfield(TRIM(scalar(l)%name)//"y"//TRIM(scalar(l)%name)&
+            //"y"//"_AVG", comp_txtx_avg)
+            ! TzTz_AVG
+            CALL register_statfield(TRIM(scalar(l)%name)//"z"//TRIM(scalar(l)%name)&
+            //"z"//"_AVG", comp_txtx_avg)
+
+            ! UT_AVG
+            CALL register_statfield("U"//TRIM(scalar(l)%name)//"_AVG", comp_ut_avg)
+            ! VT_AVG
+            CALL register_statfield("V"//TRIM(scalar(l)%name)//"_AVG", comp_ut_avg)
+            ! WT_AVG
+            CALL register_statfield("W"//TRIM(scalar(l)%name)//"_AVG", comp_ut_avg)
+
+            !UTT_AVG
+            CALL register_statfield("U"//TRIM(scalar(l)%name)&
+            //TRIM(scalar(l)%name)//"_AVG", comp_utt_avg)
+            !VTT_AVG
+            CALL register_statfield("V"//TRIM(scalar(l)%name)&
+            //TRIM(scalar(l)%name)//"_AVG", comp_utt_avg)
+            !WTT_AVG
+            CALL register_statfield("W"//TRIM(scalar(l)%name)&
+            //TRIM(scalar(l)%name)//"_AVG", comp_utt_avg)
+
+        END DO
+
+    END SUBROUTINE init_scastat
 
     SUBROUTINE finish_scastat
         CONTINUE
     END SUBROUTINE finish_scastat
+
+    !Routine to compute the TxTx_AVG, TyTy_AVG and TzTz_AVG fields
+
+    SUBROUTINE  comp_txtx_avg(field, name, dt)
+        ! Subroutine arguments
+        TYPE(field_t), INTENT(inout) :: field
+        CHARACTER(len=*), INTENT(in) :: name
+        REAL(realk), INTENT(in) :: dt
+
+        !Local Variables
+        TYPE(field_t) :: tx_f ! It can be the derivative of a scalar in any direction
+        ! The units of the scalar are asummed to be °K (Temperature)
+        INTEGER(intk), PARAMETER :: units(*) = [0, -2, 0, 2, 0, 0, 0] 
+        INTEGER(intk), PARAMETER :: units_tx(*) = [0, -1, 0, 1, 0, 0, 0]
+        TYPE(field_t), POINTER :: t_f
+        INTEGER(intk) :: istag, jstag, kstag
+        CHARACTER(len=3) :: ivar
+        CHARACTER(len=nchar_name) :: name_tx
+        INTEGER(intk) :: nchar
+        INTEGER(intk) :: sca_name_length
+        CHARACTER(len=nchar_name) :: sca_name
+
+        nchar = LEN_TRIM(name)
+        sca_name_length = (nchar-6)/2 
+        sca_name = name(1:sca_name_length)
+        name_tx = name(1:sca_name_length+1)
+
+        CALL get_field(t_f, sca_name)
+
+        istag = 0
+        jstag = 0
+        kstag = 0
+
+        SELECT CASE (TRIM(name(nchar-4:nchar)))
+        CASE ("x_AVG")
+            ivar = "DXT"
+        CASE ("y_AVG")
+            ivar = "DYT"
+        CASE ("z_AVG")
+            ivar = "DZT"
+        CASE DEFAULT
+            CALL errr(__FILE__, __LINE__)
+        END SELECT
+
+        CALL field%init(name, istag=istag, jstag=jstag, kstag=kstag, &
+            units=units)
+        CALL tx_f%init(name, istag=istag, jstag=jstag, kstag=kstag, &
+            units=units_tx)
+        CALL differentiate(tx_f, t_f, ivar)
+
+        field%arr = tx_f%arr(:)**2
+
+        CALL tx_f%finish()
+
+    END SUBROUTINE
+
+    SUBROUTINE comp_ut_avg(field, name, dt)
+
+        !Subroutine arguments
+        TYPE(field_t), INTENT(inout) :: field
+        CHARACTER(len=*), INTENT(in) :: name
+        REAL(realk), INTENT(in) :: dt
+
+        ! Local variables
+        INTEGER(intk), PARAMETER :: units(*) = [0, 1, -1, 1, 0, 0, 0]
+        TYPE(field_t), POINTER :: u_f, t_f
+        INTEGER(intk) :: istag, jstag, kstag
+        INTEGER(intk) :: sca_name_length
+        INTEGER(intk) :: nchar
+        CHARACTER(len=nchar_name) :: sca_name
+
+        nchar = LEN_TRIM(name)        
+        sca_name_length = nchar-5
+        sca_name = name (2:sca_name_length+1)
+
+        SELECT CASE (name(1:1))
+        CASE ("U")
+            CALL get_field(u_f, "U")
+            istag = 1
+            jstag = 0
+            kstag = 0
+        CASE ("V")
+            CALL get_field(u_f, "V")
+            istag = 0
+            jstag = 1
+            kstag = 0
+        CASE ("W")
+            CALL get_field(u_f, "W")
+            istag = 0
+            jstag = 0
+            kstag = 1
+        CASE DEFAULT
+            CALL errr(__FILE__, __LINE__)
+        END SELECT
+
+        CALL get_field(t_f, sca_name)
+
+        CALL field%init(name, istag=istag, jstag=jstag, kstag=kstag, &
+            units=units)
+
+        ! Multiplication of U and the scalar (T) 
+        CALL field%multiply(u_f, t_f)
+
+    END SUBROUTINE comp_ut_avg
+
+    SUBROUTINE comp_utt_avg(field, name, dt)
+       ! Subroutine arguments
+        TYPE(field_t), INTENT(inout) :: field
+        CHARACTER(len=*), INTENT(in) :: name
+        REAL(realk), INTENT(in) :: dt
+
+        !Local variables
+        INTEGER(intk), PARAMETER :: units(*) = [0, 1, -1, 2, 0, 0, 0]
+        TYPE(field_t), POINTER :: u_f, t_f
+        INTEGER(intk) :: istag, jstag, kstag
+        INTEGER(intk) :: sca_name_length
+        CHARACTER(len=nchar_name) :: sca_name
+        INTEGER(intk) :: nchar
+
+        nchar = LEN_TRIM(name)        
+        sca_name_length = (nchar-5)/2
+        sca_name = name(2:sca_name_length+1)
+
+        SELECT CASE (name(1:1))
+        CASE ("U")
+            CALL get_field(u_f, "U")
+            istag = 1
+            jstag = 0
+            kstag = 0
+        CASE ("V")
+            CALL get_field(u_f, "V")
+            istag = 0
+            jstag = 1
+            kstag = 0
+        CASE ("W")
+            CALL get_field(u_f, "W")
+            istag = 0
+            jstag = 0
+            kstag = 1
+        CASE DEFAULT
+            CALL errr(__FILE__, __LINE__)
+        END SELECT
+
+        CALL get_field(t_f, sca_name)
+
+        CALL field%init(name, istag=istag, jstag=jstag, kstag=kstag, &
+            units=units)
+
+        ! Multiplication considering staggeredness
+        CALL field%multiply(u_f, t_f, t_f)
+    
+    END SUBROUTINE
+
 END MODULE scastat_mod
