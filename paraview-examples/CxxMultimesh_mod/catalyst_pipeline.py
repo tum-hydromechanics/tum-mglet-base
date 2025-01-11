25bce2ac880ef6ce5e6d1d978c531d832da024d5
ge27den <eduardo.quintana@tum.de>
2025-01-11 14:00:59 +0100

    adding some files

--- paraview-examples/CxxMultimesh_mod/catalyst_pipeline.py
+++ paraview-examples/CxxMultimesh_mod/catalyst_pipeline.py
@@ -15,30 +15,33 @@ def catalyst_execute(info):
 
     gridInfo = producer.GetSubsetDataInformation(0, "//grid", "Hierarchy");
     particlesInfo = producer.GetSubsetDataInformation(0, "//particles", "Hierarchy");
-
     print("-----------------------------------")
-#     print("executing (cycle={}, time={})".format(info.cycle, info.time))
-#     print("grid:")
-#     print("  bounds:", gridInfo.GetBounds())
-#     print("  velocity-range:",
-#             gridInfo.GetPointDataInformation().GetArrayInformation("velocity").GetComponentRange(0))
-#     print("  pressure-range:",
-#             gridInfo.GetCellDataInformation().GetArrayInformation("pressure").GetComponentRange(0))
-#     print("  author:",
-#             gridInfo.GetFieldDataInformation().GetArrayInformation("author").GetRangesAsString())
-#     print("  mesh time:",
-#             gridInfo.GetFieldDataInformation().GetArrayInformation("mesh time").GetComponentRange(0))
-#     print("  mesh timestep:",
-#             gridInfo.GetFieldDataInformation().GetArrayInformation("mesh timestep").GetComponentRange(0))
-#     print("  mesh external data:",
-#             gridInfo.GetFieldDataInformation().GetArrayInformation("mesh external data").GetComponentRange(0))
+    print("executing (cycle={}, time={})".format(info.cycle, info.time))
+    print("grid:")
+    print("  bounds:", gridInfo.GetBounds())
+    print("  velocity-range:",
+            gridInfo.GetPointDataInformation().GetArrayInformation("velocity").GetComponentRange(0))
+    print("  pressure-range:",
+            gridInfo.GetCellDataInformation().GetArrayInformation("pressure").GetComponentRange(0))
+    print("  author:",
+            gridInfo.GetFieldDataInformation().GetArrayInformation("author").GetRangesAsString())
+    print("  mesh time:",
+            gridInfo.GetFieldDataInformation().GetArrayInformation("mesh time").GetComponentRange(0))
+    print("  mesh timestep:",
+            gridInfo.GetFieldDataInformation().GetArrayInformation("mesh timestep").GetComponentRange(0))
+    print("  mesh external data:",
+            gridInfo.GetFieldDataInformation().GetArrayInformation("mesh external data").GetComponentRange(0))
 
-#     print("particles:")
-#     print("  bounds:", particlesInfo.GetBounds())
+    print("particles:")
+    print("  bounds:", particlesInfo.GetBounds())
 
-#     # try subsetting using the assembly and confirm it's as exepected.
-#     gridInfo2 = producer.GetSubsetDataInformation(0, "//Grid", "Assembly")
-#     assert gridInfo2.GetNumberOfPoints() == gridInfo.GetNumberOfPoints()
+    # try subsetting using the assembly and confirm it's as exepected.
+    gridInfo2 = producer.GetSubsetDataInformation(0, "//Grid", "Assembly")
+    assert gridInfo2.GetNumberOfPoints() == gridInfo.GetNumberOfPoints()
 
-#     info2 = producer.GetSubsetDataInformation(0, "//SubCollection", "Assembly")
-#     assert producer.GetDataInformation().GetNumberOfDataSets() == info2.GetNumberOfDataSets()
+    info2 = producer.GetSubsetDataInformation(0, "//SubCollection", "Assembly")
+    assert producer.GetDataInformation().GetNumberOfDataSets() == info2.GetNumberOfDataSets()
+
+
+    # producer.relay.io.save(grid)
+    paraview.simple.io.SaveData("pythonsave1", grid)
