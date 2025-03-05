e3e72fcf71e8ee6f86973caa2afd735514bf24c9
Simon Wenczowski <simon.wenczowski@tum.de>
2025-03-05 15:04:42 +0100

    adding oversub

--- tests/Restart/run.sh
+++ tests/Restart/run.sh
@@ -8,10 +8,10 @@ ACTION=$1
 if [[ "$ACTION" == "test" ]]; then
     MGLET_BIN=$2
     cp parameters_start.json parameters.json
-    mpirun -n 32 $MGLET_BIN 2>&1 | tee mglet_01.OUT
+    mpirun --oversubscribe -n 32 $MGLET_BIN 2>&1 | tee mglet_01.OUT
     mv fields.h5 fields_started.h5
     cp parameters_continue.json parameters.json
-    mpirun -n 32 $MGLET_BIN 2>&1 | tee mglet_02.OUT
+    mpirun --oversubscribe -n 32 $MGLET_BIN 2>&1 | tee mglet_02.OUT
     rm parameters.json
 elif [[ "$ACTION" == "clean" ]]; then
     rm -rf LOGS fields.h5 mglet-perf-report.txt *.OUT *.stl
