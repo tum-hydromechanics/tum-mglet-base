fd6c5c237c9bdd919c36905c17d971d88d82f2e4
Julius Gollor <julius.gollor@gmail.com>
2026-03-20 21:30:51 +0100

    bugfix for  initial particle generation (distribute_particles)

--- src/particles/particle_list_mod.F90
+++ src/particles/particle_list_mod.F90
@@ -418,9 +418,12 @@ CONTAINS    !===================================
 
             CALL RANDOM_NUMBER(grid_rn)
 
-            grid_counter = 1
-            DO WHILE (grid_rn > my_grid_volume_fractions(grid_counter))
-                grid_counter = grid_counter + 1
+            grid_counter = SIZE(my_grid_volume_fractions)
+            DO j = 1, nmygridslvl(particle_level)
+                IF (grid_rn <= my_grid_volume_fractions(j)) THEN
+                    grid_counter = j
+                    EXIT
+                END IF
             END DO
 
             igrid = mygridslvl(grid_counter, particle_level)
